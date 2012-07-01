393e7cf0c606ced7f74446596f64da4a8c3f55a1
Lei Xu <eddyxu@gmail.com>
2012-06-30 20:09:32 -0700

    + clear_cache in perftest

--- pyro/__init__.py
+++ pyro/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 
-__doc__ = 'A collection of daily used python codes'
-__author__ = 'Lei Xu <eddyxu@gmail.com>
+"""A collection of daily used python codes"""
+
+__author__ = 'Lei Xu <eddyxu@gmail.com>'
--- pyro/perftest.py
+++ pyro/perftest.py
@@ -1,5 +1,29 @@
 # Useful routines for doing performance testing.
-# Lei Xu <eddyxu@gmail.com>
+# Author: Lei Xu <eddyxu@gmail.com>
 
-import os
+"""Performance Test related functions.
+"""
+
 import sys
+import platform
+import osutil
+from subprocess import check_call as call
+
+def clear_cache():
+    """Dump all dirty data and clear file system cache
+    (including directory cache)..
+    """
+    osutil.check_root_or_exit('No enough privilege to clear cache')
+    system = platform.system()
+    if system == 'Linux':
+        status = call('sync', shell=True)
+        if status:
+            print >> sys.stderr, 'clear_cache: error on do sync'
+            return -1
+        status = call('echo 3 > /proc/sys/vm/drop_caches', shell=True)
+        if status:
+            print >> sys.stderr, 'clear_cache: error on drop caches'
+    else:
+        print >> sys.stderr, \
+            'Error: clear_cache(): unsupported system: %s' % system
+        sys.exit(1)
